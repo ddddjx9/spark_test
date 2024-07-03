6d06c05578a9a2e7bf3fe37a5849a79cc6937682
45live <694375330@qq.com>
2024-07-03 19:08:16 +0800

    update

--- SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operate/Spark10_Transform_KV_WordCount_GroupByKey.java
+++ SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operate/Spark10_Transform_KV_WordCount_GroupByKey.java
@@ -1,11 +1,12 @@
 package cn.edu.ustb.spark.rdd.operate;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.ArrayList;
 
 public class Spark10_Transform_KV_WordCount_GroupByKey {
     public static void main(String[] args) {
@@ -15,15 +16,30 @@ public class Spark10_Transform_KV_WordCount_GroupByKey {
 
         final JavaSparkContext jsc = new JavaSparkContext(conf);
 
-        jsc.parallelizePairs(Arrays.asList(new Tuple2<>("a", 1), new Tuple2<>("b", 2), new Tuple2<>("a", 3), new Tuple2<>("b", 4))).groupByKey().mapValues(tuple -> {
-            int sum = 0;
-            final Iterator<Integer> iterator = (Iterator<Integer>) tuple.iterator();
-            while (iterator.hasNext()) {
-                final Integer num = iterator.next();
-                sum += num;
+        final ArrayList<Tuple2<String, Integer>> datas = new ArrayList<>();
+
+        datas.add(new Tuple2<>("a", 1));
+        datas.add(new Tuple2<>("a", 2));
+        datas.add(new Tuple2<>("a", 3));
+        datas.add(new Tuple2<>("b", 4));
+
+        final JavaRDD<Tuple2<String, Integer>> rdd = jsc.parallelize(datas);
+        final JavaPairRDD<String, Integer> mapRDD = rdd.mapToPair(tuple -> tuple);
+
+        //将分组聚合功能进行简化操作
+        /*mapRDD.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1, Integer v2) throws Exception {
+                return v1+v2;
             }
-            return sum;
-        });
+        });*/
+
+        //运行结果：
+        //(a,4)
+        //(b,6)
+        //reduceByKey的作用：按照相同的Key来做reduce
+        //将KV类型的数据按照key对value进行reduce操作，将多个值聚合成一个值
+        mapRDD.reduceByKey(Integer::sum).collect().forEach(System.out::println);
 
         jsc.close();
     }
