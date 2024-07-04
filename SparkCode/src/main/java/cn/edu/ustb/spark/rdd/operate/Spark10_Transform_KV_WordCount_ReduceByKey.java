257c6f8c7702abada8b586a8cb13ce8ff872bdf2
45live <694375330@qq.com>
2024-07-04 08:27:13 +0800

    update

--- SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operate/Spark10_Transform_KV_WordCount_ReduceByKey.java
+++ SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operate/Spark10_Transform_KV_WordCount_ReduceByKey.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class Spark10_Transform_KV_WordCount_ReduceByKey {
     public static void main(String[] args) {
         final SparkConf conf = new SparkConf();
-        conf.setMaster("local[*]");
+        //conf.setMaster("local[*]");
         conf.setAppName("spark");
 
         final JavaSparkContext jsc = new JavaSparkContext(conf);
