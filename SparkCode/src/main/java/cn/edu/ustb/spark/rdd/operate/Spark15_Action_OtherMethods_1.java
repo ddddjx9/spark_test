e353799ceb66ecd4e26bce7e41994bfb90121e0a
45live <694375330@qq.com>
2024-07-05 19:53:48 +0800

    update

---
+++ SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operate/Spark15_Action_OtherMethods_1.java
@@ -0,0 +1,24 @@
+package cn.edu.ustb.spark.rdd.operate;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Spark15_Action_OtherMethods_1 {
+    public static void main(String[] args) {
+        final SparkConf conf = new SparkConf();
+        conf.setMaster("local[*]");
+        conf.setAppName("Action_OtherMethod");
+
+        final JavaSparkContext jsc = new JavaSparkContext(conf);
+
+        final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        final JavaRDD<Integer> rdd = jsc.parallelize(list, 2);
+
+
+        jsc.close();
+    }
+}
