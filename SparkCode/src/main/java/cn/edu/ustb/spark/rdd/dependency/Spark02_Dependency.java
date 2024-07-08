4f3784dc6c0a2de7621cc1c0b9f2d822a1a50d61
45live <694375330@qq.com>
2024-07-08 17:17:17 +0800

    show partitions numbers how to calculate

--- SparkCode/src/main/java/cn/edu/ustb/spark/rdd/dependency/Spark02_Dependency.java
+++ SparkCode/src/main/java/cn/edu/ustb/spark/rdd/dependency/Spark02_Dependency.java
@@ -9,7 +9,7 @@ import scala.Tuple2;
 import java.util.ArrayList;
 
 public class Spark02_Dependency {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         final SparkConf conf = new SparkConf();
         conf.setMaster("local[*]");
         conf.setAppName("spark");
@@ -21,25 +21,29 @@ public class Spark02_Dependency {
         datas.add(new Tuple2<>("a", 1));
         datas.add(new Tuple2<>("a", 2));
         datas.add(new Tuple2<>("a", 3));
-        datas.add(new Tuple2<>("b", 4));
+        datas.add(new Tuple2<>("a", 4));
 
-        final JavaRDD<Tuple2<String, Integer>> rdd = jsc.parallelize(datas);
+        final JavaRDD<Tuple2<String, Integer>> rdd = jsc.parallelize(datas, 2);
 
         //将Java版本的RDD转换为Scala版本的RDD
-        System.out.println(rdd.rdd().dependencies());
+//        System.out.println(rdd.rdd().dependencies());
 
         final JavaPairRDD<String, Integer> mapRDD = rdd.mapToPair(tuple -> tuple);
 
         //List(org.apache.spark.OneToOneDependency@4acb2510) - 窄依赖
-        System.out.println("*********************************");
-        System.out.println(mapRDD.rdd().dependencies());
+//        System.out.println("*********************************");
+//        System.out.println(mapRDD.rdd().dependencies());
 
         final JavaPairRDD<String, Integer> reduceRDD = mapRDD.reduceByKey(Integer::sum);
 
         //List(org.apache.spark.ShuffleDependency@10cd6753) - 宽依赖
-        System.out.println("*********************************");
-        System.out.println(reduceRDD.rdd().dependencies());
-        reduceRDD.collect().forEach(System.out::println);
+//        System.out.println("*********************************");
+//        System.out.println(reduceRDD.rdd().dependencies());
+        reduceRDD.collect();//.forEach(System.out::println);
+        System.out.println("计算完毕");
+
+        //http://localhost:4040
+        Thread.sleep(10000000L);
 
         jsc.close();
     }
