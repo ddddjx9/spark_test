dd05205c7d72cac1199f9c3bb483482a2362aad3
45live <694375330@qq.com>
2024-07-10 17:18:26 +0800

    show how to use new frame - SparkSQL

---
+++ SparkCode/src/main/java/cn/edu/ustb/spark/sparkSQL/SparkSQL02_Env.java
@@ -0,0 +1,35 @@
+package cn.edu.ustb.spark.sparkSQL;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class SparkSQL02_Env {
+    public static void main(String[] args) {
+        //TODO 构建环境对象
+        //     Spark在结构化数据的处理过程中对核心功能，环境进行了封装
+        //      采用构建器模式构建环境对象
+        //      构建器模式：构建对象，将构建的过程进行封装，提供外置的接口进行调用，传递参数
+        final SparkSession sparkSession = SparkSession
+                .builder()
+                .master("local[*]")
+                .appName("SparkSQL")
+                .getOrCreate();
+
+        //TODO SparkSQL对数据模型也进行了封装，将之前的RDD封装成了Dataset
+        //      对接文件数据源时，会将文件中的一行数据封装为Row对象
+        final Dataset<Row> ds = sparkSession.read().json("datas\\user.json");
+
+        //TODO 将数据模型转换为表，方便SQL的使用
+        ds.createOrReplaceTempView("user");
+
+        //TODO 使用SQL文的当时来操作数据
+        String sql = "select avg(age) from user";
+        final Dataset<Row> sqlDS = sparkSession.sql(sql);
+
+        //TODO 展示数据模型的效果
+        sqlDS.show();
+
+        sparkSession.close();
+    }
+}
