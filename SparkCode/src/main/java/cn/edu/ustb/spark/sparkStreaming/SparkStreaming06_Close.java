c0897555442d629b03a303f066f8b81531f325fe
45live <694375330@qq.com>
2024-07-15 17:22:53 +0800

    show how to close the stream gracefully

--- SparkCode/src/main/java/cn/edu/ustb/spark/sparkStreaming/SparkStreaming06_Close.java
+++ SparkCode/src/main/java/cn/edu/ustb/spark/sparkStreaming/SparkStreaming06_Close.java
@@ -19,17 +19,18 @@ public class SparkStreaming06_Close {
         jsc.start();
 
         //close就是用于关闭资源，但是不能在主线程中关闭 => 换个线程
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //关闭环境，释放资源
-                try {
-                    //因为刚开启就关闭显然不适合，所以等到开启3s（一个采集周期）后再关闭
-                    Thread.sleep(3000L);
-                    jsc.close();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+        //强制关闭
+        new Thread(() -> {
+            //关闭环境，释放资源
+            try {
+                //因为刚开启就关闭显然不适合，所以等到开启3s（一个采集周期）后再关闭
+                Thread.sleep(3000L);
+                //优雅地关闭
+                jsc.stop(true, true);
+                //强制关闭
+                //jsc.stop();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }).start();
         //表示当前的代码执行到此处后，会让主线程阻塞
