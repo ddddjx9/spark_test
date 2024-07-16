2d935d2cc46f86521c59c712d0439975b270c506
45live <694375330@qq.com>
2024-07-16 10:38:38 +0800

    Import dependencies to make it easier to view the source code; update the syntax in Scala.

---
+++ ScalaBasic/src/main/scala/cn/edu/ustb/basicOperate/ForStepSize.scala
@@ -0,0 +1,48 @@
+package cn.edu.ustb.basicOperate
+
+/**
+ * 演示循环步长的用法
+ */
+object ForStepSize {
+  def main(args: Array[String]): Unit = {
+    //循环步长
+    //默认循环步长为1
+    for (i <- 1 to 10) {
+      println(i)
+    }
+
+    //to和by底层都是调用Range方法里面的内容
+    println("********************")
+    for (i: Int <- 2.to(10, 2)) {
+      println(i)
+    }
+
+    println("********************")
+    for (i <- 1 to 10 by 2) {
+      println(i)
+    }
+
+    println("********************")
+    for (i <- Range(1, 10, 3)) {
+      println(i)
+    }
+
+    //演示循环步长为负的情况
+    println("********************")
+    for (i <- 13 to 2 by -5) {
+      println(i)
+    }
+
+    println("********************")
+    for (i <- 2 to 12 reverse) {
+      println(i)
+    }
+
+    //循环步长不能为0！
+
+    //演示循环步长为小数的情况，这种情况下，起始和终止都得为小数
+    for (data <- 1.0 to 6.0 by 0.5) {
+      println(data)
+    }
+  }
+}
