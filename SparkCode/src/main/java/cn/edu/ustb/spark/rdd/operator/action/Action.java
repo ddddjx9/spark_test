18b8792341065ec5da874ee310c4dbcac702b6b3
i-dongjiaxin <i-dongjiaxin@stepfun.com>
2024-12-31 21:14:04 +0800

    update files

--- SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operator/action/Action.java
+++ SparkCode/src/main/java/cn/edu/ustb/spark/rdd/operator/action/Action.java
@@ -17,10 +17,10 @@ public class Action {
 
         final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        //这里的collect就是RDD的行动算子，只有调用这个方法之后，数据才会流转起来
+        // 这里的collect就是RDD的行动算子，只有调用这个方法之后，数据才会流转起来
         //      RDD的行动算子会触发Job的执行
         //      调用一次行动算子，就会触发一次作业的执行
-        //注释掉collect方法之后，发现控制台上没有任何内容输出
+        // 注释掉collect方法之后，发现控制台上没有任何内容输出
         final JavaRDD<Integer> rdd = jsc.parallelize(list).map(num -> {
             System.out.println("num = " + num);
             return num * 2;
@@ -29,8 +29,8 @@ public class Action {
 //        rdd.collect().forEach(System.out::println);
 //        rdd.collect();
 
-        //行动算子和转换算子的区别？ - 产生新的Job？错！
-        //转换算子的功能：将旧的RDD转换成新的RDD，所以调用完算子之后，会有新的RDD产生
+        // 行动算子和转换算子的区别？ - 产生新的Job？错！
+        // 转换算子的功能：将旧的RDD转换成新的RDD，所以调用完算子之后，会有新的RDD产生
         final JavaRDD<Integer> sortRDD = rdd.sortBy(num -> num, true, 2);
 
         //对执行结果生成相应变量后，发现生成的不是新的RDD，而是具体的执行结果
